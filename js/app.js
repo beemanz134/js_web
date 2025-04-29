6352fac0da010fbf8706a107196fda0f2b1b0529
beemanz134 <beeman134@gmail.com>
2025-04-28 20:13:27 -0500

    toast

--- js/app.js
+++ js/app.js
@@ -11,5 +11,17 @@ function formSubmit() {
     document.getElementById("input").value = "";
 }
 
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        document.getElementById("toast_body").innerHTML = JSON.stringify(list);
+        toast.show()
+    })
+}
+
+
+
 document.getElementById("alert").addEventListener("click", alertList);
 document.getElementById("submit").addEventListener("click", formSubmit);
